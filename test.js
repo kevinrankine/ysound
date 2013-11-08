a586834d17e87d0bbd5f8dc84d8a1e1701b97507
Kevin Akos Rankine <kevinranks@gmail.com>
2013-11-08 05:01:19 +0000

    removed bullshit test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-var ffmpeg = require('fluent-ffmpeg');
-var fs = require('fs');
-var converter = new ffmpeg({ source : 'Kendrick Lamar - M.A.A.D. City (Feat. MC eiht)-10yrPDf92hY.mp4'} )
-    .withVideoBitrate(1024)
-    .withAudioBitrate('128k')
-    .toFormat('mp3')
-    .writeToStream(fs.createWriteStream('fuck.mp3'));
-console.log("Here!");
-
