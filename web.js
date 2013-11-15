1e0e92979df2b831a6e52ec10d7fbefacf7e0a3b
Kevin Akos Rankine <kevinranks@gmail.com>
2013-11-15 03:51:39 +0000

    error handling

--- web.js
+++ web.js
@@ -7,43 +7,46 @@ var app = express();
 var dl = require('ytdl');
 var ffmpeg = require('fluent-ffmpeg');
 
+app.set("view engine", "jade");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
 app.get("/favicon.ico", function (req, res) {
     res.sendfile("favicon.ico");
 });
 
 app.get('/videos/:videoID', function (req, res) {
     var videoID = req.params.videoID;
-    console.log(videoID);
     if (videoID == "undefined") {
 	res.send("Error!");
 	return;
     }
     var videoURL = "https://www.youtube.com/watch?v=" + videoID;
-    try {
-	var videoStream = dl(videoURL);
-	var converter = new ffmpeg({source : videoStream, timeout: 3000})
-	    .withVideoBitrate(1024)
-	    .withAudioBitrate('128k')
-	    .toFormat('mp3')
-	    .writeToStream(res, function (retcode, err) {
-		if (err) {
-		    console.log(err);
-		}
-		else {
-		    console.log("The conversion pipe succeeded!");
-		}
-		res.end();
-		dl.cache = {};
-	    });
-    }
-    catch(err) {
-	console.log("Response failed!");
-	res.send("Response failed!");
-    }
+    var videoStream = dl(videoURL);
+
+    var converter = new ffmpeg({source : videoStream})
+	.withVideoBitrate(1024)
+	.withAudioBitrate('128k')
+	.toFormat('mp3')
+	.writeToStream(res, function (retcode, err) {
+	    if (err) {
+		console.log(err);
+	    }
+	    else {
+		console.log("The conversion pipe succeeded!");
+	    }
+	    res.end();
+	});
 });
 app.get('/', function (req, res) {
     var videoID = req.query.q;
-    res.send('<audio controls src=/videos/' + videoID + '></audio>');
+    res.render("index", {"videoID" : videoID});
 });
+
+process.on('uncaughtException', function (err) {
+    // handles the unexpected stream closure error...for now
+});
+
+
 
 app.listen(8080);
