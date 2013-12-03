306518c204d26911945e0d1ac4582817341d647f
Kevin Akos Rankine <kevinranks@gmail.com>
2013-12-03 21:48:58 +0000

    clusters eat up memory like a bitch

--- web.js
+++ web.js
@@ -8,69 +8,86 @@ var dl = require('ytdl');
 var ffmpeg = require('fluent-ffmpeg');
 var rest = require("restler")
 var cheerio = require("cheerio");
-
-app.set("view engine", "jade");
-app.set("views", __dirname + "/views");
-app.use(express.static(__dirname + "/public"));
-
-app.get("/favicon.ico", function (req, res) {
-    res.sendfile("favicon.ico");
-});
-
-app.get('/videos/:videoID', function (req, res) {
-    var videoID = req.params.videoID;
-    if (videoID == "undefined") {
-	res.send("Video ID is not valid.");
-	return;
+var cluster = require("cluster");
+var numNodes = 5;
+if (cluster.isMaster) {
+    for(var i = 0; i < numNodes; i++) {
+	cluster.fork();
     }
-    res.writeHead(200, {"Content-Type" : "audio/mp3", "Content-Length" : 1000000 * 10});
-    var videoURL = "https://www.youtube.com/watch?v=" + videoID;
-    var videoStream = dl(videoURL);
-    var converter = new ffmpeg({source : videoStream, timeout: 300})
-	.withVideoBitrate(1024)
-	.withAudioBitrate('128k')
-	.toFormat('mp3')
-	.writeToStream(res, function (retcode, err) {
-	    console.log(retcode);
-	    if (err) {
-		console.log("A pipe closed.");
-		res.end()
-	    }
-	    else {
-		console.log("The conversion pipe succeeded!");
-	    }
-	});
-});
-app.get('/watch', function (req, res) {
-    var videoID = req.query.v;
-    var searchQuery = req.query.q;
+    cluster.on('disconnect', function(worker) {
+	console.error('disconnect!');
+	cluster.fork();
+    });
+    setInterval(function () {
+	for(var i = 0; i < numNodes - cluster.workers.length; i++) {
+	    cluster.fork();
+	}
+    }, 3000);
+}
+else {
+    app.set("view engine", "jade");
+    app.set("views", __dirname + "/views");
+    app.use(express.static(__dirname + "/public"));
 
-    if (videoID) {
-	res.render("index", {"videoID" : videoID});
-    }
-    else if (searchQuery) {
-	var listingURL = "http://youtube.com/results?search_query=";
-	listingURL += searchQuery.split(" ").join("+");
-	console.log("The listing url is ", listingURL);
-	
-	rest.get(listingURL).on("complete", function (data) {
-	    if (data instanceof Error) {
-		res.end("Couldn't load page.");
-		return;
-	    }
-	    $ = cheerio.load(data);
-	    var listingData = $("a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2");
-	    res.render("index", {"listingData" : listingData});
-	});
-    }
-    else {
-	res.render("index");
-    }
-});
+    app.get("/favicon.ico", function (req, res) {
+	res.sendfile("favicon.ico");
+    });
 
-app.listen(process.env.PORT || 80);
-/*
-process.on("uncaughtException", function (err) {
-    console.log(err.message);
-});
-*/
+    app.get('/videos/:videoID', function (req, res) {
+	var videoID = req.params.videoID;
+	if (videoID == "undefined") {
+	    res.send("Video ID is not valid.");
+	    return;
+	}
+	res.writeHead(200, {"Content-Type" : "audio/mp3", "Content-Length" : 1000000 * 10});
+	var videoURL = "https://www.youtube.com/watch?v=" + videoID;
+	var videoStream = dl(videoURL);
+	var converter = new ffmpeg({source : videoStream, timeout: 300})
+	    .withVideoBitrate(1024)
+	    .withAudioBitrate('128k')
+	    .toFormat('mp3')
+	    .writeToStream(res, function (retcode, err) {
+		console.log(retcode);
+		if (err) {
+		    console.log("A pipe closed.");
+		    res.end()
+		}
+		else {
+		    console.log("The conversion pipe succeeded!");
+		}
+	    });
+    });
+    app.get('/watch', function (req, res) {
+	var videoID = req.query.v;
+	var searchQuery = req.query.q;
+
+	if (videoID) {
+	    res.render("index", {"videoID" : videoID});
+	}
+	else if (searchQuery) {
+	    var listingURL = "http://youtube.com/results?search_query=";
+	    listingURL += searchQuery.split(" ").join("+");
+	    console.log("The listing url is ", listingURL);
+	    
+	    rest.get(listingURL).on("complete", function (data) {
+		if (data instanceof Error) {
+		    res.end("Couldn't load page.");
+		    return;
+		}
+		$ = cheerio.load(data);
+		var listingData = $("a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2");
+		res.render("index", {"listingData" : listingData});
+	    });
+	}
+	else {
+	    res.render("index");
+	}
+    });
+
+    app.listen(process.env.PORT || 80);
+    /*
+      process.on("uncaughtException", function (err) {
+      console.log(err.message);
+      });
+    */
+}
