45dda70d157a15c85e8e21f54ebf1871164c6eb7
Ubuntu <ubuntu@ip-10-157-51-138.ec2.internal>
2013-12-07 01:53:03 +0000

    setup script added

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install g++ curl libssl-dev apache2-utils
+sudo apt-get install git-core
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+curl https://npmjs.org/install.sh | sudo sh
+sudo apt-get install ffmpeg libavcodec-extra-53
