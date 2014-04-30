1c03de2367a68eab817ed9662c55a68a03105dc5
Kevin Akos Rankine <kevinranks@gmail.com>
2014-04-30 14:56:53 +0000

    setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install g++ curl libssl-dev apache2-utils
-sudo apt-get install git-core
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install g++ curl libssl-dev apache2-utils
+sudo apt-get -y install git-core
+sudo apt-get -y update
 sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install nodejs
+sudo apt-get -y update
+sudo apt-get -y install nodejs
 curl https://npmjs.org/install.sh | sudo sh
-sudo apt-get install ffmpeg libavcodec-extra-53
+sudo apt-get -y install ffmpeg libavcodec-extra-53
